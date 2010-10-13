cf1b1245d65a745abe23d50077d17d8cac6474ad
ramu_ramamurthy <ramu_ramamurthy@yahoo.com>
2010-10-12 22:53:02 -0700

    add ui for rename button

--- socialcalcworkbookcontrol.js
+++ socialcalcworkbookcontrol.js
@@ -26,13 +26,16 @@ SocialCalc.WorkBookControl = function(book, divid, defaultsheetname) {
     this.sheetCnt = 0;
     this.numSheets = 0;
     this.currentSheetButton = null;
+	this.renameDialogId = "sheetRenameDialog";
+	
 	this.sheetshtml = '<div id="fooBar" style="padding:6px;background-color:#80A9F3;"></div>';
 
 	this.buttonshtml = 
 '<form>'+
 '<div id="workbookControls" style="padding:6px;background-color:#80A9F3;">'+
 '<input type="button" value="add sheet" onclick="SocialCalc.WorkBookControlAddSheet(true)" class="smaller">'+
-'<input type="button" value="del sheet" onclick="SocialCalc.WorkBookControlDelSheet()" class="smaller">'+
+'<input type="button" value="delete sheet" onclick="SocialCalc.WorkBookControlDelSheet()" class="smaller">'+
+'<input type="button" value="rename sheet" onclick="SocialCalc.WorkBookControlRenameSheet()" class="smaller">'+
 '<input type="button" value="save workbook" onclick="SocialCalc.WorkBookControlSaveSheet()" class="smaller">'+
 '</div>'+
 '</form>';
@@ -167,4 +170,83 @@ SocialCalc.WorkBookControlSaveSheet = function(){
 	
 	control.workbook.CreateSaveWorkBook();
 	
+}
+
+SocialCalc.WorkBookControlRenameSheet = function(){
+
+	var control = SocialCalc.GetCurrentWorkBookControl();
+	
+	// do a popup to get the new name of the sheet
+	// the popup has an input element with submit, and cancel buttons
+	var	element = document.getElementById(control.renameDialogId);
+   if (element) return;
+   
+   var currentsheet = control.currentSheetButton.id;
+   var str = '<div style="padding:6px 0px 4px 6px;">'+
+         '<span style="font-size:smaller;">'+'Rename-'+ currentsheet + '</span><br>'+
+         '<input type="text" id="newSheetName" style="width:380px;" value="'+'"><br>'+'</div>';
+
+   str +='<div style="width:380px;text-align:right;padding:6px 0px 4px 6px;font-size:small;">'+
+         '<input type="button" value="Submit" style="font-size:smaller;" onclick="SocialCalc.WorkBookControlRenameSheetSubmit();">&nbsp;'+
+         '<input type="button" value="Cancel" style="font-size:smaller;" onclick="SocialCalc.WorkBookControlRenameSheetHide();"></div>';
+
+   var main = document.createElement("div");
+   main.id = control.renameDialogId;
+
+   main.style.position = "absolute";
+
+   var vp = SocialCalc.GetViewportInfo();
+
+   main.style.top = (vp.height/3)+"px";
+   main.style.left = (vp.width/3)+"px";
+   main.style.zIndex = 100;
+   main.style.backgroundColor = "#FFF";
+   main.style.border = "1px solid black";
+
+   main.style.width = "400px";
+
+   main.innerHTML = '<table cellspacing="0" cellpadding="0" style="border-bottom:1px solid black;"><tr>'+
+      '<td style="font-size:10px;cursor:default;width:100%;background-color:#999;color:#FFF;">'+"&nbsp;"+'</td>'+
+      '<td style="font-size:10px;cursor:default;color:#666;" onclick="SocialCalc.WorkBookControlRenameSheetHide();">&nbsp;X&nbsp;</td></tr></table>'+
+      '<div style="background-color:#DDD;">'+str+'</div>';
+
+	//alert(main.innerHTML);
+
+   SocialCalc.DragRegister(main.firstChild.firstChild.firstChild.firstChild, true, true, {MouseDown: SocialCalc.DragFunctionStart, MouseMove: SocialCalc.DragFunctionPosition,
+                  MouseUp: SocialCalc.DragFunctionPosition,
+                  Disabled: null, positionobj: main});
+
+   control.workbook.spreadsheet.spreadsheetDiv.appendChild(main);
+
+   ele = document.getElementById("newSheetName");
+   ele.focus();
+   SocialCalc.CmdGotFocus(ele);
+   
+}
+
+SocialCalc.WorkBookControlRenameSheetHide = function(){
+
+   var control = SocialCalc.GetCurrentWorkBookControl();
+   var spreadsheet = control.workbook.spreadsheet;
+
+   var ele = document.getElementById(control.renameDialogId);
+   ele.innerHTML = "";
+
+   SocialCalc.DragUnregister(ele);
+
+   SocialCalc.KeyboardFocus();
+
+   if (ele.parentNode) {
+      ele.parentNode.removeChild(ele);
+   }
+}
+
+SocialCalc.WorkBookControlRenameSheetSubmit = function(){
+
+   // this handles all the rename action
+   var ele = document.getElementById("newSheetName");
+   //alert(ele.value);
+   var control = SocialCalc.GetCurrentWorkBookControl();
+   control.currentSheetButton.value = ele.value;
+   SocialCalc.WorkBookControlRenameSheetHide();
 }
